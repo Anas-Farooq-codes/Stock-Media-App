06b95e42b7b33a55c5f1086f5c55bf644e60e9e8
hitmananas <anasfarooq2006@gmail.com>
2024-09-05 19:57:07 +0500

    structured and styled photo detail page

--- pages/photos/photo_detail.js
+++ pages/photos/photo_detail.js
@@ -1,3 +1,12 @@
 
 
 "use strict";
+
+// IMPORTS 
+
+import {client} from "../../api.js";
+import {ripple} from "../../ripple.js";
+import {gridInit, updateGrid} from "../../masonry_grid.js";
+import {photoCard} from "../../photo_card.js";
+import {menu} from "../../menu.js";
+import {favorite} from "../../favorite.js";
